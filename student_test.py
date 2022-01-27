df8284a3d96c00ccf8fbd41dab243f8e0fa66af1
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2022-01-26 20:06:06 -0500

    Update student_test.py

--- student_test.py
+++ student_test.py
@@ -3,8 +3,8 @@ import student
 
 
 
-def test_inputs():
-    input_values=['3','4']
+def test_jump():
+    input_values=['jump']
     output=[]
 
     def mock_input(s=None):
@@ -20,15 +20,60 @@ def test_inputs():
 
     student.main()
 
-    assert output[2]==7
+    assert output[1]=='jumping'
 
+def test_eat():
+    input_values=['eat']
+    output=[]
 
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+    
+    student.input = mock_input
+    student.print = lambda s : output.append(s)
 
+    student.main()
 
-#Standard test output ONLY (no input)
-#No need for the if __name__ condition in the main code, but might as well when done so students get used to it.
-"""def test_hello(capsys):
-    import hello
-    out,err = capsys.readouterr()
-    assert out == "Hello world!\n" or "bye" in out, "Should read 'Hello world!' "
-"""
+    assert output[1]=='eating'    
+
+def test_scarps():
+    input_values=['scarp']
+    output=[]
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+    
+    student.input = mock_input
+    student.print = lambda s : output.append(s)
+
+    student.main()
+
+    assert output[1]=='scarping'
+def test_groundbreak():
+    input_values=['groundbreak']
+    output=[]
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+    
+    student.input = mock_input
+    student.print = lambda s : output.append(s)
+
+    student.main()
+
+    assert output[1]=='groundbreaking'
