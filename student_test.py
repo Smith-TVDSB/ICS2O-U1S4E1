28bef846509ad8b22054c190ed3375759e9c24f7
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2022-02-25 19:16:52 -0500

    Fixed print styles test

--- student_test.py
+++ student_test.py
@@ -2,78 +2,154 @@ import pytest
 import student 
 
 
-
 def test_jump():
-    input_values=['jump']
-    output=[]
+    try:
+        input_values=['jump']
+        output=[]
 
-    def mock_input(s=None):
-        if s is not None:
-            output.append(s)
-            return input_values.pop(0)
-        else:
-            output.append("")
-            return input_values.pop(0)
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
     
-    student.input = mock_input
-    student.print = lambda s : output.append(s)
+        student.input = mock_input
+        student.print = lambda s : output.append(s)
 
-    student.main()
+        student.main()
 
-    assert output[1]=='jumping'
+        assert output[1]=='jumping'
+
+    except:
+        input_values=['jump']
+        output=[]
+
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        student.input = mock_input
+        student.print = lambda s,t : output.extend([s,t])
+
+        student.main()
+
+        assert output[1]+output[2][-3:]=='jumping'
 
 def test_eat():
-    input_values=['eat']
-    output=[]
+    try:
+        input_values=['eat']
+        output=[]
 
-    def mock_input(s=None):
-        if s is not None:
-            output.append(s)
-            return input_values.pop(0)
-        else:
-            output.append("")
-            return input_values.pop(0)
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        
+        student.input = mock_input
+        student.print = lambda s : output.append(s)
+
+        student.main()
+
+        assert output[1]=='eating' 
     
-    student.input = mock_input
-    student.print = lambda s : output.append(s)
+    except:
+        input_values=['eat']
+        output=[]
 
-    student.main()
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        student.input = mock_input
+        student.print = lambda s,t : output.extend([s,t])
 
-    assert output[1]=='eating'    
+        student.main()
+
+        assert output[1]+output[2][-3:]=='eating'   
 
 def test_scarps():
-    input_values=['scarp']
-    output=[]
+    try:
+        input_values=['scarp']
+        output=[]
 
-    def mock_input(s=None):
-        if s is not None:
-            output.append(s)
-            return input_values.pop(0)
-        else:
-            output.append("")
-            return input_values.pop(0)
-    
-    student.input = mock_input
-    student.print = lambda s : output.append(s)
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        
+        student.input = mock_input
+        student.print = lambda s : output.append(s)
 
-    student.main()
+        student.main()
 
-    assert output[1]=='scarping'
+        assert output[1]=='scarping'
+
+    except:
+        input_values=['scarp']
+        output=[]
+
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        student.input = mock_input
+        student.print = lambda s,t : output.extend([s,t])
+
+        student.main()
+
+        assert output[1]+output[2][-3:]=='scarping'
+
 def test_groundbreak():
-    input_values=['groundbreak']
-    output=[]
+    try:
+        input_values=['groundbreak']
+        output=[]
 
-    def mock_input(s=None):
-        if s is not None:
-            output.append(s)
-            return input_values.pop(0)
-        else:
-            output.append("")
-            return input_values.pop(0)
-    
-    student.input = mock_input
-    student.print = lambda s : output.append(s)
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        
+        student.input = mock_input
+        student.print = lambda s : output.append(s)
 
-    student.main()
+        student.main()
 
-    assert output[1]=='groundbreaking'
+        assert output[1]=='groundbreaking'
+    except:
+        input_values=['groundbreak']
+        output=[]
+
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        student.input = mock_input
+        student.print = lambda s,t : output.extend([s,t])
+
+        student.main()
+
+        assert output[1]+output[2][-3:]=='groundbreaking'
+
